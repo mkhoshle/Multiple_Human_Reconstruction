566bfadb481014c6682109222077574f9ccf428d
mkhoshle <mkhoshle@umich.edu>
2023-03-23 20:02:11 -0400

    remove HumanObj-old

--- HumanObj-old/__init__.py
+++
@@ -1,5 +0,0 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from . import _init_paths_
-
-import sys
-print("In module products sys.path[0], __package__ ==", sys.path[0], __package__)
